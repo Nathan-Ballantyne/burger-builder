b7a57188760fa8a7dccc9584cf9451daff2315b1
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-08-09 17:26:08 +1200

    load orders from firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -7,34 +7,39 @@ class Orders extends Component {
     state = {
         orders: [],
         loading: true,
-    }
+    };
 
     componentDidMount() {
-        axios.get('orders.json')
-        .then(response => {
-            const fetchedOrders = [];
-            for (let key in response.data) {
-                fetchedOrders.push({
-                    ...response.data[key],
-                    id: key,
-                });
-            };
-            this.setState({loading: false, orders: fetchedOrders});
-        })
-        .catch(error => {
-            this.setState({ loading: false });
-        });
+        axios
+            .get('orders.json')
+            .then((response) => {
+                const fetchedOrders = [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        id: key,
+                    });
+                }
+                this.setState({ loading: false, orders: fetchedOrders });
+            })
+            .catch((error) => {
+                this.setState({ loading: false });
+            });
     }
 
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map((order) => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price}
+                    />
+                ))}
             </div>
         );
     }
 }
 
 export default withErrorHandler(Orders, axios);
-
