41d677ef61a9a86ac7bc5cd31b0a9a31d3dd18eb
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-08-16 17:28:22 +1200

    add validation rules for email and zip code

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -46,6 +46,7 @@ class ContactData extends Component {
                     required: true,
                     minLength: 4,
                     maxLength: 4,
+                    isNumeric: true,
                 },
                 valid: false,
                 touched: false,
@@ -72,6 +73,7 @@ class ContactData extends Component {
                 value: '',
                 validation: {
                     required: true,
+                    isEmail: true
                 },
                 valid: false,
                 touched: false,
@@ -120,17 +122,30 @@ class ContactData extends Component {
 
     checkValidity(value, rules) {
         let isValid = true;
-
+        if (!rules) {
+            return true;
+        }
+        
         if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
 
         if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
+            isValid = value.length >= rules.minLength && isValid
         }
 
         if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
         }
 
         return isValid;
@@ -199,6 +214,6 @@ const mapStateToProps = state => {
         ings: state.ingredients,
         price: state.totalPrice,
     }
-}
+};
 
 export default connect(mapStateToProps)(ContactData);
