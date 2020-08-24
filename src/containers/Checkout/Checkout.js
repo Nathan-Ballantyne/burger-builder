22bafb5e7bda055809aa5ea2355f7df97c6fc1ef
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-08-24 20:10:14 +1200

    add redirect to inprove ux and remove error

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-   
     checkoutCanceledledHandler = () => {
         this.props.history.goBack();
     };
@@ -15,26 +14,30 @@ class Checkout extends Component {
     };
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCanceledledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}
-                />
-                <Route
-                    path={this.props.match.path + '/contact-data'}
-                    component={ContactData}
-                />
-            </div>
-        );
+        let summary = <Redirect to='/' />;
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCanceledledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}
+                    />
+                    <Route
+                        path={this.props.match.path + '/contact-data'}
+                        component={ContactData}
+                    />
+                </div>
+            );
+        }
+        return summary;
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         ings: state.ingredients,
-    }
-}
+    };
+};
 
 export default connect(mapStateToProps)(Checkout);
